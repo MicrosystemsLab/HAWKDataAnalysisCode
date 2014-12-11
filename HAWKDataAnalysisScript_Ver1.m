3c29ba9aa8e2b66834aad21d84b6b602525a971b
emazzochette <emazzochette@gmail.com>
2014-12-10 18:14:42 -0800

    Correcting for a more differences between mac/pc
    The slash is in the opposite direction. This game isn't really that fun.

--- HAWKDataAnalysisScript_Ver1.m
+++ HAWKDataAnalysisScript_Ver1.m
@@ -15,29 +15,39 @@ if (ispc) %if on PC workstation in MERL 223
     addpath(genpath('C:\Users\HAWK\Documents\HAWKDataAnalysisCode\YAMLMatlab_0.4.3'));
     excelFile = 'C:\Users\HAWK\Dropbox\HAWK\HAWKExperimentLog.xls';
     addpath('C:\Users\HAWK\Documents\HAWKDataAnalysisCode\20130227_xlwrite');
+    slash = '\';
+    % For excel writing, need these files linked:
+    % Initialisation of POI Libs
+    % Add Java POI Libs to matlab javapath
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\poi-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\poi-ooxml-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\poi-ooxml-schemas-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\xmlbeans-2.3.0.jar');
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\dom4j-1.6.1.jar');
+    javaaddpath('20130227_xlwrite\20130227_xlwrite\poi_library\stax-api-1.0.1.jar');
 elseif (ismac) % if on Eileen's personal computer
     DestinationFolder = '/Users/emazzochette/Documents/MicrosystemsResearch/HAWK/Data';
     addpath(genpath('/Users/emazzochette/Documents/MicrosystemsResearch/HAWK/HAWKDataAnalysisCode/HAWKDataAnalysisCode/YAMLMatlab_0.4.3'));
     excelFile = '/Users/emazzochette/Dropbox/HAWK/HAWKExperimentLog.xls';
     addpath('/Users/emazzochette/Documents/MicrosystemsResearch/HAWK/HAWKDataAnalysisCode/HAWKDataAnalysisCode/20130227_xlwrite');
+    slash = '/';
+    % For excel writing, need these files linked:
+    % Initialisation of POI Libs
+    % Add Java POI Libs to matlab javapath
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-ooxml-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-ooxml-schemas-3.8-20120326.jar');
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/xmlbeans-2.3.0.jar');
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/dom4j-1.6.1.jar');
+    javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/stax-api-1.0.1.jar');
 end
-
-% For excel writing, need these files linked:
-% Initialisation of POI Libs
-% Add Java POI Libs to matlab javapath
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-3.8-20120326.jar');
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-ooxml-3.8-20120326.jar');
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/poi-ooxml-schemas-3.8-20120326.jar');
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/xmlbeans-2.3.0.jar');
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/dom4j-1.6.1.jar');
-javaaddpath('20130227_xlwrite/20130227_xlwrite/poi_library/stax-api-1.0.1.jar');
 
 %asks user for the directory where all the files are:
 directory = uigetdir(DestinationFolder,'Choose the folder where the data if located');
 
 %determine experiment title based on file name:
 for index = length(directory):-1:1
-    if directory(index) == '/'
+    if (directory(index) == slash)
         startTitleIndex = index+1;
         break;
     end

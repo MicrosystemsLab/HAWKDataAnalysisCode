915d9e2f1edf6ba8226d18dd5ae22519f3e21947
emazzochette <emazzochette@gmail.com>
2014-12-17 20:14:24 -0800

    Added Random Permutations Generator
    Added a script to generate the locations and magnitudes in a random
    order.

---
+++ randomPermuations.m
@@ -0,0 +1,6 @@
+
+locations = [15 20 25 30 35 40 45 50 55 60 65 70 75 80 85];
+locations_rand = locations(randperm(length(locations)))
+
+magnitudes = [1000 900 800 700 600 500 400 300 200 100];
+magnitudes_rand = magnitudes(randperm(length(magnitudes)))

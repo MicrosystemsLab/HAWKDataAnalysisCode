54460005551018395372a06f8c1376be713c5a21
emazzochette <emazzochette@gmail.com>
2015-04-02 21:39:27 -0700

    More frame filtering development

--- scoreTrials.m
+++ scoreTrials.m
@@ -10,20 +10,56 @@
         
         % count number of frames above residue limit
         
+        %find frames that have 
+        for i = 1:length(skeleton)
+            lengthOfSkeleton = length(skeleton(i).x);
+            
+            [x0, y0] = intersections(skeleton(i).x(1:floor(0.9*lengthOfSkeleton)), skeleton(i).y(1:floor(0.9*lengthOfSkeleton)));
+            numberOfIntersections(i) = length(x0);
+        end
+        
     end
 % end
 
 
 %%
 for stim = 1:numStims
+    
+    skeleton = Stimulus(stim).Skeleton;
+    clear numberOfIntersections;
+    for i = 1:length(skeleton)
+            lengthOfSkeleton = length(skeleton(i).x);
+            
+            [x0, y0] = intersections(skeleton(i).x(1:floor(0.9*lengthOfSkeleton)), skeleton(i).y(1:floor(0.9*lengthOfSkeleton)));
+            numberOfIntersections(i) = length(x0);
+    end
+    ind = find(numberOfIntersections>0);
+    
     subplot(2,3,stim)
+          
+    % Phase shift plot:
     semilogy(Stimulus(stim).phaseShift.residual)
-    hold on
+   
+    %Dropped frames based on length calculation
     droppedFrames = zeros(length(Stimulus(stim).numFrames));
     droppedFrames(Stimulus(stim).droppedFrames) = 0.1;
     semilogy(droppedFrames,'rx');
     
-    semilogy(Stimulus(stim).FramesByStimulus.DuringStimFrames,0.5.*ones(size(Stimulus(stim).FramesByStimulus.DuringStimFrames)),'gx')
+    %Show frames during stimulus:
+    semilogy(Stimulus(stim).FramesByStimulus.DuringStimFrames,0.75.*ones(size(Stimulus(stim).FramesByStimulus.DuringStimFrames)),'gx')
+    
+    %frames that have the skeleton cross itself:
+    semilogy(ind,ones(size(ind))*0.25,'mx')
+    
+    %Plot manually scored frames:
+    semilogy(Stimulus(stim).manuallyScoredBadFrames-Stimulus(stim).ProcessedFrameNumber(1)+1, 0.9*ones(size(Stimulus(stim).manuallyScoredBadFrames)),'bo')
+    semilogy(Stimulus(stim).manuallyScoredOmegaTurns-Stimulus(stim).ProcessedFrameNumber(1)+1, 0.9*ones(size(Stimulus(stim).manuallyScoredOmegaTurns)),'ro')
+    
+    %Look at width of worm:
+    wideWorms = find(Stimulus(stim).BodyMorphology.widthAtTarget>150);
+    semilogy(wideWorms, ones(size(wideWorms))*0.6,'gx')
+    
+    
     axis([0 300 10^-6 10^0])
     title(['Stimulus: ' num2str(stim)])
 end
